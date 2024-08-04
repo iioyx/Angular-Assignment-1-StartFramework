5b9532a5cd8b062ab23064085b6fb2da2e3347c9
Yousef ibrahim <108943127+iioyx@users.noreply.github.com>
2024-08-04 09:51:02 +0300

    Edit route config

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TAASVWKH.mjs";import"./chunk-MOD5CYON.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-D4OSEG23.mjs";import"./chunk-IJOWP4NC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
